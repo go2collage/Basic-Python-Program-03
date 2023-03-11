4ec5aec51702fa888e0cd99c0978cc52a5cd1ac7
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:26:08 +0530

    Add files via upload

---
+++ BP_P3.py
@@ -0,0 +1,9 @@
+# Basic Python Program
+
+# square root of a number
+
+a = int(input("Enter a integer number: "))
+
+a = a ** 2      # square root using Exponentiation (**) operator 
+
+print("square root is: ", a)
